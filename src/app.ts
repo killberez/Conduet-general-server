a38e99e5c28daf6f45ca4eeff0eb70d983cc3058
Taras Liush <taras.liush@gmail.com>
2021-02-28 11:50:19 +0200

    1st commit

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ import projectRouter from "./routes/project/Project.route";
 import userRouter from "./routes/user/User.route";
 import authRouter from "./routes/Auth";
 import bodyParser from "body-parser";
-import helmet from 'helmet';
+import helmet from "helmet";
 import cors from "cors";
 import authMiddleware from "./middlewares/auth";
 import * as dotenv from "dotenv";
@@ -14,14 +14,11 @@ const app = express();
 dotenv.config();
 
 mongoose
-  .connect(
-    process.env.MONGODB_CONNECT_LINK as string,
-    {
-      useNewUrlParser: true,
-      useCreateIndex: true,
-      useUnifiedTopology: true,
-    }
-  )
+  .connect(process.env.MONGODB_CONNECT_LINK as string, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {})
   .catch((err: Error) => {
     console.log(
@@ -29,18 +26,16 @@ mongoose
     );
   });
 
-app.set("port", 8000);
+app.set("port", 8000 || process.env.PORT);
 
 app.use(helmet());
 app.use(cors());
-
-
 
 app.use(bodyParser.json());
 
 app.use("/auth", authRouter);
 
-app.use("/api",authMiddleware);
+app.use("/api", authMiddleware);
 app.use("/api/v1/project", projectRouter);
 app.use("/api/v1/user", userRouter);
 
